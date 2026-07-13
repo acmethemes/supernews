98c14c73df5b2fb6ec318d62e364697b0dcff86c
codersantosh <codersantosh@gmail.com>
2026-07-13 20:49:02 +0000

    Remove deprecated IE conditional script tags and update translation template

--- acmethemes/core.php
+++ acmethemes/core.php
@@ -132,10 +132,8 @@ function supernews_scripts() {
 
 	/*jquery start*/
 	wp_enqueue_script( 'html5shiv', get_template_directory_uri() . '/assets/library/html5shiv/html5shiv.min.js', array( 'jquery' ), '3.7.3', false );
-	wp_script_add_data( 'html5shiv', 'conditional', 'lt IE 9' );
 
 	wp_enqueue_script( 'respond', get_template_directory_uri() . '/assets/library/respond/respond.min.js', array( 'jquery' ), '1.1.2', false );
-	wp_script_add_data( 'respond', 'conditional', 'lt IE 9' );
 
 	wp_enqueue_script( 'bxslider', get_template_directory_uri() . '/assets/library/bxslider/js/jquery.bxslider.js', array( 'jquery' ), '4.2.5.1', 1 );
 
